69af34ec049747123acb59b89c0ac272093ab49e
Dan Fletcher <dannyjfletcher@outlook.com>
2021-02-10 22:22:53 -0500

    Basic Drawing

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const canvas = /** @type {HTMLCanvasElement} */ (document.querySelector(
   'canvas',
 ))
 const ctx = /** @type {CanvasRenderingContext2D} */ (canvas.getContext('2d'))
+
+ctx.fillStyle = 'yellow'
+ctx.fillRect(200, 100, 100, 50)
+
+ctx.strokeStyle = 'red'
+ctx.strokeRect(400, 100, 100, 50)
+
+ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+ctx.fillStyle = 'white'
+ctx.font = ' 24px serif'
+ctx.textAlign = 'center'
+ctx.fillText('Canvas cleared!! 🎉', canvas.width / 2, canvas.height / 2)
