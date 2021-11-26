b835bd07b22c945719397c891f8d7e56ab532384
Dan Fletcher <dannyjfletcher@outlook.com>
2021-11-26 09:03:39 -0500

    Add delta time

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 import { Ball } from './modules/entities/ball.js'
 
-const canvas = /** @type {HTMLCanvasElement} */ (document.querySelector(
-  'canvas',
-))
+const canvas = /** @type {HTMLCanvasElement} */ (
+  document.querySelector('canvas')
+)
 const ctx = /** @type {CanvasRenderingContext2D} */ (canvas.getContext('2d'))
 
 const ball = new Ball({
   width: 12,
   height: 12,
   position: { x: 100, y: 100 },
-  velocity: { x: 5, y: 5 },
+  velocity: { x: 300, y: 300 },
 })
 
-function frame() {
-  ball.position.x += ball.velocity.x
-  ball.position.y += ball.velocity.y
+let dt = 0
+let last = performance.now()
+
+/**
+ * The game loop.
+ * @param {DOMHighResTimeStamp} hrt
+ */
+function frame(hrt) {
+  // How much time has elapsed since last frame?
+  // Convert time to seconds
+  dt = (hrt - last) / 1000
+
+  // Account for delta time by applying it to the ball's velocity
+  ball.position.x += ball.velocity.x * dt
+  ball.position.y += ball.velocity.y * dt
 
   if (ball.position.x + ball.width > canvas.width) {
     ball.position.x = canvas.width - ball.width
@@ -36,6 +48,9 @@ function frame() {
 
   ctx.fillStyle = 'white'
   ctx.fillRect(ball.position.x, ball.position.y, ball.width, ball.height)
+
+  // Track the last time for the delta time calculation in the subsequent frame.
+  last = hrt
 
   requestAnimationFrame(frame)
 }
